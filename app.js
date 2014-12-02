7ecc30154045332c95f61cee2086f67bb3ed8c81
Rushi Shah <2016rshah@gmail.com>
2014-12-02 17:54:06 -0500

    Made the routes and stuff work so it is a full fledged web app

--- app.js
+++ app.js
@@ -2,16 +2,8 @@ app = require('express.io')()
 app.http().io()
 var randomizer = require('./randomizer')
 var names;
-randomizer.getNames('1zlqvP1NIlx1mVQ2ruyARQyzuSLIWvHX2xz_r0bMbAiY', function(data){
-	names = data
-	console.log("Data randomized")
-})
-
-
-
-
-// Setup the ready route, and emit talk event.
-app.io.route('ready', function(req) {
+app.io.route('gotNames', function(req) {
+	console.log("got here")
 	var givers = [];
 	for(var i = 0; i<names.length; i++){
 		givers.push(names[i].first + " " + names[i].last)
@@ -22,19 +14,20 @@ app.io.route('ready', function(req) {
 		reciever = names[i].givingTo
 		recievers.push(reciever.first+ " " + reciever.last)
 	}
-	console.log("Recievers: ", recievers)
-    req.io.emit('givers', {
-        message: givers
-    })
-    req.io.emit('recivers', {
-    	message: recievers
+    req.io.emit("giversAndRecievers", {
+    	message: [givers, recievers]
     })
 })
-
-// Send the client html.
+app.io.route('gotDocumentId', function(req){
+	console.log("Id: ", req.data)
+	randomizer.getNames(req.data, function(data){
+		names = data
+		console.log("Data randomized")
+		req.io.route("gotNames")
+	})
+})
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/client.html')
 })
-
 app.listen(3000)
 console.log("App listening on 3000")
